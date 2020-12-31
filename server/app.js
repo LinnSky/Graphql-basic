ad9ceb7a9e2f7b2680cefd37a9ce5d54f2e11ca9
Linn Fang <linn_fang@Linns-iMac.local>
2020-12-30 19:26:26 -0800

    graphql with database

--- server/app.js
+++ server/app.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// connect to mlab database
+mongoose
+ .connect(
+  "mongodb+srv://Fang:fdl8382291@gql-books-library.dusio.mongodb.net/books?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+ )
+ .then(() => console.log("Connected to MongoDB Atlas"))
+ .catch(err => console.log("Error: ", err.message));
+
 
 app.use('/graphql', graphqlHTTP({
     schema,
